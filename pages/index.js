bb16a7247ad03229ba31f6c77017e76be0c5c2da
LinaDev14 <perfilgitlinamaria@gmail.com>
2022-08-12 20:01:13 -0500

    se instala en el proyecto tailwind

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}></main>
-
-      <footer className={styles.footer}>
-      </footer>
+      <main></main>
     </div>
   )
 }
